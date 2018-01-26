0099ea73ecb3aa2940bc2f1dc56a5192516cead1
Gabriel Perez <gabrielmpp2@gmail.com>
2018-01-26 15:02:32 -0200

    site do curso

--- build_site.R
+++ build_site.R
@@ -2,6 +2,7 @@
 #This helps avoid confusion if our working directory is 
 #not our site because of other projects we were 
 #working on at the time. 
+
 setwd("/media/gabriel/Novo volume/Curso-R/curso-R/")
 
 #render your sweet site. 
